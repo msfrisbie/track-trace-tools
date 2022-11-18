3275a3acfc305efd0e6a9b7fc840fe6dc07f6311
Matt Frisbie <msfrisbie@gmail.com>
2022-11-18 10:57:48 -0600

    fix timezone bug

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,30 +1,32 @@
 export function todayIsodate(): string {
-    return getIsoDateStringFromIsoDatetime((new Date()).toISOString());
+  // get today's date in the user's timezone but formatted as ISO8601 (YYYY-MM-DD)
+  // This works because the Sweden locale uses the ISO 8601 format.
+  return new Date().toLocaleDateString("sv");
 }
 
 export function nowIsotime(): string {
-    return getIsoTimeStringFromIsoDatetime((new Date()).toISOString());
+  return new Date().toLocaleTimeString("sv") + ".000Z";
 }
 
 export function submitDateFromIsodate(isodate: string): string {
-    // 2021-06-05
-    const [year, month, day] = isodate.split('-');
+  // 2021-06-05
+  const [year, month, day] = isodate.split("-");
 
-    return `${month}/${day}/${year}`
+  return `${month}/${day}/${year}`;
 }
 
 export function getIsoDateStringFromIsoDatetime(isoDatetime: string): string {
-    if (!isoDatetime.includes('T')) {
-        throw new Error('This does not look like an isoDatetime')
-    }
+  if (!isoDatetime.includes("T")) {
+    throw new Error("This does not look like an isoDatetime");
+  }
 
-    return isoDatetime.split('T')[0];
+  return isoDatetime.split("T")[0];
 }
 
 export function getIsoTimeStringFromIsoDatetime(isoDatetime: string): string {
-    if (!isoDatetime.includes('T')) {
-        throw new Error('This does not look like an isoDatetime')
-    }
+  if (!isoDatetime.includes("T")) {
+    throw new Error("This does not look like an isoDatetime");
+  }
 
-    return isoDatetime.split('T')[1];
+  return isoDatetime.split("T")[1];
 }
