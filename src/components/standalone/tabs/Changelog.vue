acd9494bf568dea462f68fca63ca56003ab22cd2
Matt Frisbie <msfrisbie@gmail.com>
2023-02-10 11:53:27 -0600

    changelog

--- src/components/standalone/tabs/Changelog.vue
+++ src/components/standalone/tabs/Changelog.vue
@@ -2,6 +2,12 @@
   <b-tab title="Changelog"
     ><b-card-text class="flex flex-col gap-4 items-center">
       <div class="max-w-lg flex flex-col gap-4">
+        <p class="text-2xl ttt-purple">v0.17.0</p>
+
+        <ul class="list-disc">
+          <li>Added support for medical tags</li>
+        </ul>
+
         <p class="text-2xl ttt-purple">v0.14.0</p>
 
         <ul class="list-disc">
