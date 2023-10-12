ff5b4e82e16dd97468ed925f067e242728227454
Matt Frisbie <msfrisbie@gmail.com>
2023-10-11 21:53:50 -0500

    refactor

---
+++ src/utils/graph.ts
@@ -0,0 +1,259 @@
+import Graph from "graphology";
+import Sigma from "sigma";
+import { Settings } from "sigma/settings";
+import {
+  Coordinates,
+  EdgeDisplayData,
+  NodeDisplayData,
+  PartialButFor,
+  PlainObject,
+} from "sigma/types";
+
+export interface GraphData {
+  nodes: {
+    key: string;
+    attributes: {
+      size: number;
+      label: string;
+      color: string;
+    };
+  }[];
+  edges: {
+    key: string;
+    source: string;
+    target: string;
+    attributes: { type: "arrow"; label: string; size: number };
+  }[];
+}
+
+// Type and declare internal state:
+export interface State {
+  hoveredNode?: string;
+  searchQuery: string;
+
+  // State derived from query:
+  selectedNode?: string;
+  suggestions?: Set<string>;
+
+  // State derived from hovered node:
+  hoveredNeighbors?: Set<string>;
+}
+// Actions:
+export function setSearchQuery(
+  state: State,
+  graph: Graph,
+  renderer: Sigma,
+  searchInput: HTMLInputElement,
+  query: string
+) {
+  state.searchQuery = query;
+
+  if (searchInput.value !== query) {
+    searchInput.value = query;
+  }
+
+  if (query) {
+    const lcQuery = query.toLowerCase();
+    const suggestions = graph
+      // @ts-ignore
+      .nodes()
+      // @ts-ignore
+      .map((n: any) => ({ id: n, label: graph.getNodeAttribute(n, "label") as string }))
+      .filter(({ label }: { label: any }) => label.toLowerCase().includes(lcQuery));
+
+    // If we have a single perfect match, them we remove the suggestions, and
+    // we consider the user has selected a node through the datalist
+    // autocomplete:
+    if (suggestions.length === 1 && suggestions[0].label === query) {
+      state.selectedNode = suggestions[0].id;
+      state.suggestions = undefined;
+
+      // Move the camera to center it on the selected node:
+      const nodePosition = renderer.getNodeDisplayData(state.selectedNode) as Coordinates;
+      renderer.getCamera().animate(nodePosition, {
+        duration: 500,
+      });
+    }
+    // Else, we display the suggestions list:
+    else {
+      state.selectedNode = undefined;
+      state.suggestions = new Set(suggestions.map(({ id }: { id: any }) => id));
+    }
+  }
+  // If the query is empty, then we reset the selectedNode / suggestions state:
+  else {
+    state.selectedNode = undefined;
+    state.suggestions = undefined;
+  }
+
+  // Refresh rendering:
+  renderer.refresh();
+}
+
+export function setHoveredNode(graph: Graph, state: State, renderer: Sigma, node?: string) {
+  if (node) {
+    state.hoveredNode = node;
+    // @ts-ignore
+    state.hoveredNeighbors = new Set(graph.neighbors(node));
+  } else {
+    state.hoveredNode = undefined;
+    state.hoveredNeighbors = undefined;
+  }
+
+  // Refresh rendering:
+  renderer.refresh();
+}
+
+const TEXT_COLOR = "#000000";
+
+export function drawRoundRect(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  radius: number
+): void {
+  ctx.beginPath();
+  ctx.moveTo(x + radius, y);
+  ctx.lineTo(x + width - radius, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+  ctx.lineTo(x + width, y + height - radius);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+  ctx.lineTo(x + radius, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+  ctx.lineTo(x, y + radius);
+  ctx.quadraticCurveTo(x, y, x + radius, y);
+  ctx.closePath();
+}
+
+export function hoverRenderer(
+  context: CanvasRenderingContext2D,
+  data: PlainObject,
+  settings: PlainObject
+) {
+  const size = settings.labelSize;
+  const font = settings.labelFont;
+  const weight = settings.labelWeight;
+  const subLabelSize = size - 2;
+
+  const label = data.label;
+  const subLabel = data.tag !== "unknown" ? data.tag : "";
+  const clusterLabel = data.clusterLabel;
+
+  // Then we draw the label background
+  context.beginPath();
+  context.fillStyle = "#fff";
+  context.shadowOffsetX = 0;
+  context.shadowOffsetY = 2;
+  context.shadowBlur = 8;
+  context.shadowColor = "#000";
+
+  context.font = `${weight} ${size}px ${font}`;
+  const labelWidth = context.measureText(label).width;
+  context.font = `${weight} ${subLabelSize}px ${font}`;
+  const subLabelWidth = subLabel ? context.measureText(subLabel).width : 0;
+  context.font = `${weight} ${subLabelSize}px ${font}`;
+  const clusterLabelWidth = clusterLabel ? context.measureText(clusterLabel).width : 0;
+
+  const textWidth = Math.max(labelWidth, subLabelWidth, clusterLabelWidth);
+
+  const x = Math.round(data.x);
+  const y = Math.round(data.y);
+  const w = Math.round(textWidth + size / 2 + data.size + 3);
+  const hLabel = Math.round(size / 2 + 4);
+  const hSubLabel = subLabel ? Math.round(subLabelSize / 2 + 9) : 0;
+  const hClusterLabel = Math.round(subLabelSize / 2 + 9);
+
+  drawRoundRect(context, x, y - hSubLabel - 12, w, hClusterLabel + hLabel + hSubLabel + 12, 5);
+  context.closePath();
+  context.fill();
+
+  context.shadowOffsetX = 0;
+  context.shadowOffsetY = 0;
+  context.shadowBlur = 0;
+
+  // And finally we draw the labels
+  context.fillStyle = TEXT_COLOR;
+  context.font = `${weight} ${size}px ${font}`;
+  context.fillText(label, data.x + data.size + 3, data.y + size / 3);
+
+  if (subLabel) {
+    context.fillStyle = TEXT_COLOR;
+    context.font = `${weight} ${subLabelSize}px ${font}`;
+    context.fillText(subLabel, data.x + data.size + 3, data.y - (2 * size) / 3 - 2);
+  }
+
+  context.fillStyle = data.color;
+  context.font = `${weight} ${subLabelSize}px ${font}`;
+  context.fillText(clusterLabel, data.x + data.size + 3, data.y + size / 3 + 3 + subLabelSize);
+}
+
+export function labelRenderer(
+  context: CanvasRenderingContext2D,
+  data: PartialButFor<NodeDisplayData, "x" | "y" | "size" | "label" | "color">,
+  settings: Settings
+) {
+  if (!data.label) {
+    return;
+  }
+
+  const size = settings.labelSize,
+    font = settings.labelFont,
+    weight = settings.labelWeight;
+
+  context.font = `${weight} ${size}px ${font}`;
+  const width = context.measureText(data.label).width + 8;
+
+  context.fillStyle = "#ffffffcc";
+  context.fillRect(data.x + data.size, data.y + size / 3 - 15, width, 20);
+
+  context.fillStyle = "#000";
+  context.fillText(data.label, data.x + data.size + 3, data.y + size / 3);
+}
+
+// Render nodes accordingly to the internal state:
+// 1. If a node is selected, it is highlighted
+// 2. If there is query, all non-matching nodes are greyed
+// 3. If there is a hovered node, all non-neighbor nodes are greyed
+export function nodeReducer(state: State, node: string, data: any) {
+  const res: Partial<NodeDisplayData> = { ...data };
+
+  if (state.hoveredNeighbors && !state.hoveredNeighbors.has(node) && state.hoveredNode !== node) {
+    res.label = "";
+    res.color = "#f6f6f6";
+  }
+
+  if (state.selectedNode === node) {
+    res.highlighted = true;
+  } else if (state.suggestions && !state.suggestions.has(node)) {
+    res.label = "";
+    res.color = "#f6f6f6";
+  }
+
+  return res;
+}
+
+// Render edges accordingly to the internal state:
+// 1. If a node is hovered, the edge is hidden if it is not connected to the
+//    node
+// 2. If there is a query, the edge is only visible if it connects two
+//    suggestions
+export function edgeReducer(state: State, edge: string, data: any) {
+  const res: Partial<EdgeDisplayData> = { ...data };
+
+  // @ts-ignore
+  if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
+    res.hidden = true;
+  }
+
+  if (
+    state.suggestions &&
+    // @ts-ignore
+    (!state.suggestions.has(graph.source(edge)) || !state.suggestions.has(graph.target(edge)))
+  ) {
+    res.hidden = true;
+  }
+
+  return res;
+}
