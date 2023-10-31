6e0e2769a493ed569015990b715fe4e325411d64
Matt Frisbie <msfrisbie@gmail.com>
2023-10-31 09:17:13 -0500

    new eslint and circleci

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,31 +6,70 @@ module.exports = {
     webextensions: true,
   },
 
-  extends: [
-    "plugin:vue/essential",
-    // '@vue/airbnb',
-  ],
-
   parserOptions: {
+    // project: ["./tsconfig.json"],
     parser: "@typescript-eslint/parser",
   },
 
   rules: {
+    // https://stackoverflow.com/questions/34764287/turning-off-eslint-rule-for-a-specific-file
+    //
     // Need to keep consoles in build for production debugging
     // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prefer-const": "off",
+    "no-console": "off",
+    "prefer-const": "warn",
     "no-var": "off",
+    "no-await-in-loop": "off",
+    "no-loop-func": "off",
+    "import/prefer-default-export": "off",
+    "import/no-cycle": "off",
+    "no-restricted-syntax": "off",
+    "no-constant-condition": "off",
+    "no-param-reassign": "off",
+    "array-callback-return": "off",
+    "no-plusplus": "off",
+    "max-len": "off",
+    "no-unused-vars": "off",
+    "no-use-before-define": "off",
+    "no-case-declarations": "off",
+    "no-shadow": "off",
+    "no-continue": "off",
+    "import/no-extraneous-dependencies": "off",
+    camelcase: "off",
+    "no-bitwise": "off",
+    "no-empty": "off",
+    "no-undef": "off",
+    "no-useless-escape": "off",
+    "func-names": "off",
+    "no-unused-expressions": "off",
+    "no-underscore-dangle": "off",
+    "default-case": "off",
+    "no-async-promise-executor": "off",
+    "prefer-destructuring": "off",
+    "max-classes-per-file": "off",
+    "no-empty-pattern": "off",
+    "prefer-promise-reject-errors": "off",
+    "class-methods-use-this": "off",
+    "no-empty-function": "off",
+    "implicit-arrow-linebreak": "off",
+    "guard-for-in": "off",
+    "brace-style": "off",
+    "no-throw-literal": "off",
+    quotes: "off",
+    "operator-linebreak": "off",
+    "comma-dangle": "off",
   },
 
-  extends: ["plugin:vue/essential", "@vue/typescript"],
+  extends: ["plugin:vue/essential", "@vue/typescript", "@vue/airbnb"],
 
   overrides: [
     {
-      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+      files: ["**/*.spec.ts", "src/test/utils/mocks.ts"],
       env: {
         jest: true,
       },
     },
   ],
+
+  ignorePatterns: ["webpack.config.js"],
 };
